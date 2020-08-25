af2d4678fd333d301708ca95564ffb589cc9046f
esther-soyoung <esther.soyoung@gmail.com>
2020-08-26 03:20:24 +0900

    take cuda as an argument

--- run_models.py
+++ run_models.py
@@ -90,10 +90,11 @@ parser.add_argument('-t', '--timepoints', type=int, default=100, help="Total num
 parser.add_argument('--max-t',  type=float, default=5., help="We subsample points in the interval [0, args.max_tp]")
 parser.add_argument('--noise-weight', type=float, default=0.01, help="Noise amplitude for generated traejctories")
 
+parser.add_argument('--gpu', type=int, default=3, help="cuda:")
 
 args = parser.parse_args()
 
-device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:{}".format(args.gpu) if torch.cuda.is_available() else "cpu")
 file_name = os.path.basename(__file__)[:-3]
 utils.makedirs(args.save)
 
