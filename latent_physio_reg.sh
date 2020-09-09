cad72f69741b79f3acae347b5c48b02381a3605a
esther-soyoung <esther.soyoung@gmail.com>
2020-09-09 03:45:03 +0000

    load all logs

---
+++ latent_physio_reg.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+python run_models.py \
+        --niters 100 \
+        -n 8000 \
+        -l 20 \
+        --latent-ode \
+        --z0-encoder 'rnn' \
+        --dataset 'physionet' \
+        --rec-dims 40 \
+        --rec-layers 3 \
+        --gen-layers 3 \
+        --units 50 \
+        --gru-units 50 \
+        --quantization 0.016 \
+        --classif \
+	--reg_dopri $1 \
+	--reg_kinetic $2 \
+	--reg_deriv $3 \
+	--reg_l1 $4 \
+	--reg_l2 $5 \
+	--gpu 2
