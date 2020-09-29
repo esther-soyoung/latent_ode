e06815f3b3f74b89d74ae69de107385b9e4cfc9d
esther-soyoung <esther.soyoung@gmail.com>
2020-09-29 12:43:22 +0900

    run script: bugfix

--- latent_physio_step2.sh
+++ latent_physio_step2.sh
@@ -14,8 +14,8 @@ python run_models.py \
         --gru-units 50 \
         --quantization 0.016 \
         --classif \
-	--reg_dopri $1 \
-	--reg_kinetic $2 \
-	--reg_l1 $3 \
-	--reg_l2 $4 \
-	--step_size $5
+        --reg_dopri $1 \
+        --reg_kinetic $2 \
+        --reg_l1 $3 \
+        --reg_l2 $4 \
+        --step_size $5
