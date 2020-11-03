09df4c4496142c7794aaca9f81ec41f97904bd81
esther-soyoung <esther.soyoung@gmail.com>
2020-11-03 10:30:41 +0900

    save logs

---
+++ latent_physio_step2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+python train_aux.py \
+        --niters 100 \
+        -n 8000 \
+        -l 20 \
+        --latent-ode \
+        --z0-encoder 'rnn' \
+        --dataset 'physionet' \
+        --rec-dims 40 \
+        --rec-layers 3 \
+        --gen-layers 3 \
+        --units 50 \
+        --gru-units 50 \
+        --quantization 0.016 \
+        --classif \
+	--step_size 0.1 \
+	--alpha 0.01 \
+	--m 10 \
+        --load '59645'
+        #--load_aux '59645_91853' \
+        #--load_aux '59645_42889' \
